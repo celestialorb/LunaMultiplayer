beaaa955138fc92dad5c36dae9d17d9517ac7311
Christopher Busby <chris@controlant.com>
2021-10-21 16:08:21 +0000

    adding more metrics

--- Server/Web/Handlers/MetricsHandler.cs
+++ Server/Web/Handlers/MetricsHandler.cs
@@ -15,6 +15,10 @@ namespace Server.Web.Handlers {
 
     private Gauge PlayerCount;
     private Gauge SubspaceCount;
+
+    private Counter VesselEpoch;
+    private Counter VesselDistanceTravelled;
+
     private Gauge VesselAltitude;
     private Gauge VesselLatitude;
     private Gauge VesselLongitude;
@@ -22,10 +26,17 @@ namespace Server.Web.Handlers {
     private Gauge VesselEccentricity;
     private Gauge VesselInclination;
 
+    private Gauge VesselArgumentOfPeriapsis;
+    private Gauge VesselLongitudeOfAscendingNode;
+    private Gauge VesselMeanAnomaly;
+
     public MetricsHandler(ServerInformation serverInformation) {
       ServerInformation = serverInformation;
 
-      GaugeConfiguration VesselConfiguration = new GaugeConfiguration{
+      CounterConfiguration VesselCounterConfiguration = new CounterConfiguration{
+        LabelNames = new[] {"id", "name", "type"}
+      };
+      GaugeConfiguration VesselGaugeConfiguration = new GaugeConfiguration{
         LabelNames = new[] {"id", "name", "type"}
       };
 
@@ -33,31 +44,53 @@ namespace Server.Web.Handlers {
       PlayerCount = Metrics.CreateGauge("lmp_players_online_total", "The total number of players currently online.");
       SubspaceCount = Metrics.CreateGauge("lmp_subspace_total", "The total number of individual subspaces.");
 
+      // Vessel general metrics.
+      VesselDistanceTravelled = Metrics.CreateCounter("lmp_vessel_distance_travelled_meters", "The total distance travelled by the vessel.", VesselCounterConfiguration);
+      VesselEpoch = Metrics.CreateCounter("lmp_vessel_epoch_seconds", "The epoch of the vessel.", VesselCounterConfiguration);
+
       // Traditional coordinate metrics.
-      VesselAltitude = Metrics.CreateGauge("lmp_vessel_altitude_meters", "The altitude of the vessel.", VesselConfiguration);
-      VesselLatitude = Metrics.CreateGauge("lmp_vessel_latitude_degrees", "The latitude of the vessel.", VesselConfiguration);
-      VesselLongitude = Metrics.CreateGauge("lmp_vessel_longitude_degrees", "The longitude of the vessel.", VesselConfiguration);
+      VesselAltitude = Metrics.CreateGauge("lmp_vessel_altitude_meters", "The altitude of the vessel.", VesselGaugeConfiguration);
+      VesselLatitude = Metrics.CreateGauge("lmp_vessel_latitude_degrees", "The latitude of the vessel.", VesselGaugeConfiguration);
+      VesselLongitude = Metrics.CreateGauge("lmp_vessel_longitude_degrees", "The longitude of the vessel.", VesselGaugeConfiguration);
 
       // Orbital metrics.
-      VesselSemimajorAxis = Metrics.CreateGauge("lmp_vessel_semimajor_axis_meters", "The semimajor axis of the vessel's orbit.", VesselConfiguration);
-      VesselEccentricity = Metrics.CreateGauge("lmp_vessel_eccentricity", "The eccentricity of the vessel's orbit.", VesselConfiguration);
-      VesselInclination = Metrics.CreateGauge("lmp_vessel_inclination_degrees", "The inclination of the vessel's orbit.", VesselConfiguration);
+      VesselSemimajorAxis = Metrics.CreateGauge("lmp_vessel_semimajor_axis_meters", "The semimajor axis of the vessel's orbit.", VesselGaugeConfiguration);
+      VesselEccentricity = Metrics.CreateGauge("lmp_vessel_eccentricity", "The eccentricity of the vessel's orbit.", VesselGaugeConfiguration);
+      VesselInclination = Metrics.CreateGauge("lmp_vessel_inclination_degrees", "The inclination of the vessel's orbit.", VesselGaugeConfiguration);
+
+      // Orbital positional metrics.
+      VesselArgumentOfPeriapsis = Metrics.CreateGauge("lmp_vessel_argument_of_periapsis_degrees", "The vessel's argument of periapsis in its current orbit.", VesselGaugeConfiguration);
+      VesselLongitudeOfAscendingNode = Metrics.CreateGauge("lmp_vessel_longitude_of_ascending_node_degrees", "The vessel's longitude of ascending node in its current orbit.", VesselGaugeConfiguration);
+      VesselMeanAnomaly = Metrics.CreateGauge("lmp_vessel_mean_anomaly_radians", "The vessel's mean anomaly.", VesselGaugeConfiguration);
     }
 
     public Task Handle(IHttpContext context, Func<Task> next) {
+      // Set general metrics.
       PlayerCount.Set(ServerInformation.CurrentState.CurrentPlayers.Count);
       SubspaceCount.Set(ServerInformation.CurrentState.Subspaces.Count);
 
+      // Set vessel metrics.
       foreach(var vessel in ServerInformation.CurrentState.CurrentVessels) {
         var identifier = vessel.Id.ToString();
 
+        // Set vessel general metrics.
+        VesselEpoch.WithLabels(identifier, vessel.Name, vessel.Type).IncTo(vessel.Epoch);
+        VesselDistanceTravelled.WithLabels(identifier, vessel.Name, vessel.Type).IncTo(vessel.DistanceTravelled);
+
+        // Set vessel traditional coordinate metrics.
         VesselAltitude.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.Alt);
         VesselLatitude.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.Lat);
         VesselLongitude.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.Lon);
 
+        // Set vessel orbital metrics.
         VesselSemimajorAxis.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.SemiMajorAxis);
         VesselEccentricity.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.Eccentricity);
         VesselInclination.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.Inclination);
+
+        // Set vessel orbital positional metrics.
+        VesselArgumentOfPeriapsis.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.ArgumentOfPeriapsis);
+        VesselLongitudeOfAscendingNode.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.LongitudeOfAscendingNode);
+        VesselMeanAnomaly.WithLabels(identifier, vessel.Name, vessel.Type).Set(vessel.MeanAnomaly);
       }
 
       var stream = new MemoryStream();
